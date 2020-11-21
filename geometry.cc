197867e3a6a47bfeee406af8b626dc4a6cf48133
Jakub Molinski <jakub@molinski.dev>
2020-11-21 22:32:34 +0100

    Move function definitions to .cc

--- geometry.cc
+++ geometry.cc
@@ -1,10 +1,109 @@
 #include "geometry.h"
 
+#define m_assert(expr, msg) assert(((void)(msg), (expr)))
+
 Vector::Vector(const Position &p) : XYObject{p.x(), p.y()} {
 }
 
+XYObject::XYObject(Coordinate x, Coordinate y) : x_(x), y_(y) {
+}
+
+Coordinate XYObject::x() const {
+    return x_;
+}
+
+Coordinate XYObject::y() const {
+    return y_;
+}
+
+Vector::Vector(Coordinate x, Coordinate y) : XYObject{x, y} {
+}
+
+bool Vector::operator==(const Vector &other) const {
+    return x_ == other.x_ && y_ == other.y_;
+}
+
+Vector &Vector::operator+=(const Vector &other) {
+    x_ += other.x_;
+    y_ += other.y_;
+    return *this;
+}
+
+Vector Vector::operator+(const Vector &other) const {
+    return Vector(x_ + other.x_, y_ + other.y_);
+}
+
+Vector Vector::reflection() const {
+    return Vector(y_, x_);
+}
+
+Position::Position(Coordinate x, Coordinate y) : XYObject{x, y} {
+}
+
+Position::Position(const Vector &v) : XYObject{v.x(), v.y()} {
+}
+
+bool Position::operator==(const Position &other) const {
+    return x_ == other.x_ && y_ == other.y_;
+}
+
+Position Position::reflection() const {
+    return Position(y_, x_);
+}
+
+const Position &Position::origin() {
+    static Position o(0, 0);
+    return o;
+}
+
+Position &Position::operator+=(const Vector &v) {
+    x_ += v.x();
+    y_ += v.y();
+    return *this;
+}
+
+Rectangle::Rectangle(ScalarType width, ScalarType height)
+    : width_(width), height_(height), left_bottom_corner(Position{0, 0}) {
+    m_assert(height_ > 0 && width_ > 0, "Both dimensions of a rectangle must be positive.");
+}
+
+Rectangle::Rectangle(ScalarType width, ScalarType height, const Position position)
+    : width_(width), height_(height), left_bottom_corner(position) {
+    m_assert(height_ > 0 && width_ > 0, "Both dimensions of a rectangle must be positive.");
+}
+
+bool Rectangle::operator==(const Rectangle &other) const {
+    return width_ == other.width_ && height_ == other.height_ &&
+           left_bottom_corner == other.left_bottom_corner;
+}
+
+Rectangle Rectangle::reflection() const {
+    return Rectangle(height_, width_, left_bottom_corner.reflection());
+}
+
+ScalarType Rectangle::height() const {
+    return height_;
+}
+
+ScalarType Rectangle::width() const {
+    return width_;
+}
+
+uint64_t Rectangle::area() const {
+    return width_ * height_;
+}
+
+Position Rectangle::pos() const {
+    return left_bottom_corner;
+}
+
+Rectangle &Rectangle::operator+=(const Vector &v) {
+    left_bottom_corner += v;
+    return *this;
+}
+
 Position operator+(const Position &p, const Vector &v) {
-    return Position(p) += v;
+    return Position(p.x() + v.x(), p.y() + v.y());
 }
 
 Position operator+(const Vector &v, const Position &p) {
--- geometry.h
+++ geometry.h
@@ -7,130 +7,65 @@
 using ScalarType = int_fast32_t;
 using Coordinate = ScalarType;
 
-#define m_assert(expr, msg) assert(((void)(msg), (expr)))
-
 class XYObject {
   protected:
     Coordinate x_, y_;
 
   public:
-    XYObject(Coordinate x, Coordinate y) : x_(x), y_(y) {
-    }
+    XYObject(Coordinate x, Coordinate y);
 
-    Coordinate x() const {
-        return x_;
-    }
-
-    Coordinate y() const {
-        return y_;
-    }
+    Coordinate x() const;
+    Coordinate y() const;
 };
 
 class Position;
-/*
-- Vector implementuje operator "+=(vec)", gdzie vec jest obiektem klasy Vector.
-  Operator powinien zwrócić referencję do wołanego obiektu.
- */
+
 class Vector : public XYObject {
   public:
-    Vector(Coordinate x, Coordinate y) : XYObject{x, y} {
-    }
+    Vector(Coordinate x, Coordinate y);
     explicit Vector(const Position &p);
 
-    bool operator==(const Vector &other) const {
-        return x_ == other.x_ && y_ == other.y_;
-    }
+    Vector reflection() const;
 
-    Vector &operator+=(const Vector &other) {
-        x_ += other.x_;
-        y_ += other.y_;
-        return *this;
-    }
-
-    Vector &operator+(const Vector &other) const {
-        return Vector(other) += *this;
-    }
-
-    Vector reflection() const {
-        return Vector(y_, x_);
-    }
+    bool operator==(const Vector &other) const;
+    Vector &operator+=(const Vector &other);
+    Vector operator+(const Vector &other) const;
 };
 
 class Position : public XYObject {
   public:
-    Position(Coordinate x, Coordinate y) : XYObject{x, y} {
-    }
+    Position(Coordinate x, Coordinate y);
+    explicit Position(const Vector &v);
 
-    explicit Position(const Vector &v) : XYObject{v.x(), v.y()} {
-    }
+    Position reflection() const;
 
-    bool operator==(const Position &other) const {
-        return x_ == other.x_ && y_ == other.y_;
-    }
+    bool operator==(const Position &other) const;
+    Position &operator+=(const Vector &v);
 
-    Position reflection() const {
-        return Position(y_, x_);
-    }
-
-    static const Position origin() {
-        return Position(0, 0);
-    }
-
-    Position &operator+=(const Vector &v) {
-        x_ += v.x();
-        y_ += v.y();
-        return *this;
-    }
+    static const Position &origin();
 };
-
-Position operator+(const Position &, const Vector &);
-Position operator+(const Vector &, const Position &);
 
 class Rectangle {
     ScalarType width_, height_;
     Position left_bottom_corner;
 
   public:
-    Rectangle(ScalarType width, ScalarType height)
-        : width_(width), height_(height), left_bottom_corner(Position{0, 0}) {
-        m_assert(height_ > 0 && width_ > 0, "Both dimensions of a rectangle must be positive.");
-    }
+    Rectangle(ScalarType width, ScalarType height);
+    Rectangle(ScalarType width, ScalarType height, Position position);
 
-    Rectangle(ScalarType width, ScalarType height, const Position position)
-        : width_(width), height_(height), left_bottom_corner(position) {
-        m_assert(height_ > 0 && width_ > 0, "Both dimensions of a rectangle must be positive.");
-    }
+    Rectangle reflection() const;
 
-    bool operator==(const Rectangle &other) const {
-        return width_ == other.width_ && height_ == other.height_ &&
-               left_bottom_corner == other.left_bottom_corner;
-    }
+    ScalarType height() const;
+    ScalarType width() const;
+    Position pos() const;
+    uint64_t area() const; // TODO type alias?
 
-    Rectangle reflection() const {
-        return Rectangle(height_, width_, left_bottom_corner.reflection());
-    }
-
-    ScalarType height() const {
-        return height_;
-    }
-
-    ScalarType width() const {
-        return width_;
-    }
-
-    uint64_t area() const { // TODO type alias
-        return width_ * height_;
-    }
-
-    Position pos() const {
-        return left_bottom_corner;
-    }
-
-    Rectangle &operator+=(const Vector &v) {
-        left_bottom_corner += v;
-        return *this;
-    }
+    bool operator==(const Rectangle &other) const;
+    Rectangle &operator+=(const Vector &v);
 };
+
+Position operator+(const Position &, const Vector &);
+Position operator+(const Vector &, const Position &);
 
 Rectangle operator+(const Rectangle &, const Vector &);
 Rectangle operator+(const Vector &, const Rectangle &);
